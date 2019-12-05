f1b540fe777eaf77642a40eb1beeb6406fc1b9f6
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-05 13:24:52 -0500

    created a .js file and linked it to my .html page; created a little content to be displayed on the html page.

---
+++ quizjava.js
@@ -0,0 +1,36 @@
+
+var questions [
+    { 
+      title: "this will be the first question",
+      choices: ["choice a", "choice b", "choice c", "choice d"],
+      answer: ["choice a"]
+     },
+    
+     { 
+      title: "this will be the second question",
+      choices: ["choice a", "choice b", "choice c", "choice d"],
+      answer: ["choice a"]
+     },
+    
+     { 
+      title: "this will be the third question",
+      choices: ["choice a", "choice b", "choice c", "choice d"],
+      answer: ["choice a"]
+     },
+    
+     { 
+      title: "this will be the fourth question",
+      choices: ["choice a", "choice b", "choice c", "choice d"],
+      answer: ["choice a"]
+     },
+    
+     { 
+      title: "this will be the fifth question",
+      choices: ["choice a", "choice b", "choice c", "choice d"],
+      answer: ["choice a"]
+     }
+    ]
+    
+
+    //create a button to begin teh quiz. 
+    //add an event listener to begin cycling through the questions when they press the button.

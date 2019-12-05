0b304ceb6a6e04d48e60bd00bbb2c3848535cb96
Priya Mahalanobis <pmahalan@knights.ucf.edu>
2019-12-05 16:27:20 -0500

    Added some more code, though it's gotten convoluted and I might start over with a different approach...

--- quizjava.js
+++ quizjava.js
@@ -4,4 +4,24 @@ var thirdset = document.querySelector("#problem3");
 var fourthset = document.querySelector("#problem4");
 var fifthset = document.querySelector("#problem5");
 
-firstset.addEventListener("click")
+var firstcorrect = document.querySelector("#1d")
+var secondcorrect = document.querySelector("#2a")
+var thirdcorrect = document.querySelector("#3b")
+var fourthcorrect = document.querySelector("#4c")
+var fifthcorrect = document.querySelector("#5b")
+
+//write code that enables the "right" and "wrong" messages to be displayed for each question. 
+
+//For each question, 
+
+//If  the user clicks on the correct option, it'll display a "correct" message. Otherwise it'll display an incorrect message.
+firstset.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    if(event.target.matches(firstcorrect)) {
+        firstset.append("Correct!")
+    }
+    else {
+        firstset.append("Wrong!")
+    }
+}) 
